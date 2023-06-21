37cd66a56737cd88f5ad67b14c31ce8c7dd768b4
dnu22zcu <117291864+dnu22zcu@users.noreply.github.com>
2023-06-21 23:41:37 +0000

    my first commits

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,24 @@
-// Write your code here
+// Write your code here - CHECK END SOLUTION
+var Title;
+var Author;
+var ISBN;
+var totalCopies;
+
+function getAvailability(){
+  if(totalCopies == 0){
+    return "Out of stock";
+  } else if(totalCopies < 10){
+    return "Low stock";
+  } else{
+    return "In stock!";
+  }
+}
+
+function sell(numSold = 1){
+  return totalCopies -= numSold;
+}
+
+function restock(numCopies = 5){
+  return totalCopies += numCopies;
+}
+
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,23 @@
 // Write your code here
+
+class Movie{
+  constructor(Title, Director, Genre, ReleaseYear, Rating){
+      this.Title = Title;
+      this.Director = Director;
+      this.Genre = Genre;
+      this.ReleaseYear = ReleaseYear;
+      this.Rating = Rating;
+
+  }
+
+  movieOverview(){
+    return `${this.Title},a,${this.Genre} film directed
+    by ${this.Director} was released in ${this.ReleaseYear}.
+    It has a rating of ${this.Rating}`;
+  }
+  
+}
+
+const movie1 = Movie("Barbie", "Greta Gertwig", "Comedy", 2023, 8.0);
+
+movie1.movieOverview;
